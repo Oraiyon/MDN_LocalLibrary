49693e19e38fd3b26b365541a2905508a46ee6f6
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-03-19 12:10:09 +0800

    Installed & required security middlewares.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const compression = require("compression");
+const helmet = require("helmet");
+const RateLimit = require("express-rate-limit");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -30,6 +33,14 @@ main();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// Security middlewars
+app.use(compression());
+app.use(helmet());
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 20
+});
+app.use(limiter);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
