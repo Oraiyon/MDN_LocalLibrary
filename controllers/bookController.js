4b1c4733df5a0865c6319862b046d9adca441432
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-03-20 10:09:05 +0800

    For testing

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -7,22 +7,22 @@ const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
-  const [numBooks, numBookInstances, numAvailableBookInstances, numAuthors, numGenres] =
-    await Promise.all([
-      Book.countDocuments().exec(),
-      BookInstance.countDocuments().exec(),
-      BookInstance.countDocuments({ status: "Available" }).exec(),
-      Author.countDocuments().exec(),
-      Genre.countDocuments().exec()
-    ]);
+  // const [numBooks, numBookInstances, numAvailableBookInstances, numAuthors, numGenres] =
+  //   await Promise.all([
+  //     Book.countDocuments().exec(),
+  //     BookInstance.countDocuments().exec(),
+  //     BookInstance.countDocuments({ status: "Available" }).exec(),
+  //     Author.countDocuments().exec(),
+  //     Genre.countDocuments().exec()
+  //   ]);
 
   res.render("index", {
-    title: "Local Library Home",
-    book_count: numBooks,
-    book_instance_count: numBookInstances,
-    book_instance_available_count: numAvailableBookInstances,
-    author_count: numAuthors,
-    genre_count: numGenres
+    title: "Local Library Home"
+    // book_count: numBooks,
+    // book_instance_count: numBookInstances,
+    // book_instance_available_count: numAvailableBookInstances,
+    // author_count: numAuthors,
+    // genre_count: numGenres
   });
 });
 
